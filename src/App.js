f9fbc4003e42761081d10842159341076ca7d7d1
Alan Cova <alanncova@gmail.com>
2021-03-08 16:31:14 -0300

    third commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   const [inputValue, setInputValue] = useState("");
   const [pokeData, setPokeData] = useState({});
   const [isLoading, setIsLoading] = useState(true);
+  const [pokeData2, setPokeData2] = useState({});
 
   function handleChange(e) {
     const { value } = e.target;
@@ -23,16 +24,27 @@ function App() {
     setInputValue("");
     console.log(dataToJson);
     setIsLoading(false);
+
+    const getData2 = await fetch(
+      `https://pokeapi.co/api/v2/type/${inputValue}`
+    );
+    const dataToJson2 = await getData2.json();
+    setPokeData2(dataToJson2);
+    setInputValue("");
+    console.log(dataToJson2);
+    setIsLoading(false);
   }
+
+  const types = pokeData.types?.[0]?.type.name.toUpperCase();
+  const sprite = pokeData.sprites?.front_default;
 
   const dataFetched = (
     <div>
       <p>{pokeData.name}</p>
       <p>{pokeData.id}</p>
       <p>{pokeData.weight}</p>
-      <img src={pokeData.sprites?.front_default} />
-      <img src={pokeData.sprites?.front_shiny} />
-      <p> {pokeData.types?.[0]?.type.name.toUpperCase()}</p>
+      <img src={sprite} />
+      <p>{types}</p>
     </div>
   );
 
@@ -54,6 +66,7 @@ function App() {
       </button>
       {!isLoading && <div>{dataFetched}</div>}
       {isLoading && <p>Pokecargando tu Pokémon... (isLoading en true)</p>}
+      <p> {pokeData2.name?.damage_relations?.no_damage_to[0]?.name}</p>
     </div>
   );
 }
